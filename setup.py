f97cabb50000eb6b195dd6af34464d1aeee8621f
Atharva Ingle <atharvaaingle@gmail.com>
2023-08-03 12:26:42 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kaggle-utils',
+    version='0.1.0',
+    author='Atharva Ingle',
+    author_email='atharvaaingle@gmail.com',
+    url='https://github.com/Gladiator07/Kaggle-Utils',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
